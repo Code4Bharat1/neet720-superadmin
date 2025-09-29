4ff302e3a00f063a7a601f38a339d0bbb44b9726
yogu-code <yogeshrane019@gmail.com>
2025-09-29 12:44:35 +0530

    fix the adminID issuse

--- src/components/create-delete-admin/createadmin.jsx
+++ src/components/create-delete-admin/createadmin.jsx
@@ -176,14 +176,14 @@ export default function AddAdminPage() {
         role: formData.role || "admin",
       };
 
-      await axios.post(
+      const res = await axios.post(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/superadmin/createadmin`,
         payload
       );
 
       await sendEmailToAdmin(
         formData.Email,
-        formData.AdminId,
+        res.data.admin.AdminId,
         formData.PassKey,
         formData.StartDate,
         formData.ExpiryDate
